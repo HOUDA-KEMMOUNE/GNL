fe742e0460a8a6e5368e648409b4e802923a8828
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-12-17 12:35:51 +0100

    update

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 23:24:45 by hkemmoun          #+#    #+#             */
+/*   Updated: 2024/12/17 12:20:40 by hkemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+# define MAX_FD 10240
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strdup(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strchr(char *s, int c);
+char	*rest_char(char *line);
+char	*get_line(char *buffer, char *rest, int fd);
+
+#endif
